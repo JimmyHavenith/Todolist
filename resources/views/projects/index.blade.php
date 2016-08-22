9dc00fbbae9af2afb752cd5a43e51692e32cf575
Jimmy Havenith <jimhave@outlook.com>
2016-08-22 17:09:09 +0200

    Display all projects

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,3 +1,15 @@
 @extends('layout')
+
 @section('mainContent')
+  <h2>Projets</h2>
+
+  @if ( !$projects->count() )
+    Vous n'avez pas de projets
+  @else
+    <ul>
+      @foreach( $projects as $project )
+        <li><a href="{{ route('projects.show', $project->slug) }}">{{ $project->name }}</a> </li>
+      @endforeach
+    </ul>
+  @endif
 @endsection
