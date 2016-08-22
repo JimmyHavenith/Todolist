6b516afdd2f70b722ca37ae8231a71f6c2df316e
Jimmy Havenith <jimhave@outlook.com>
2016-08-22 17:19:35 +0200

    Display all tasks by project

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,3 +1,14 @@
 @extends('layout')
 @section('mainContent')
+  <h2>{{ $project->name }}</h2>
+
+  @if ( !$project->tasks->count() )
+    Ton projet n'a pas de tâches qui lui sont associées
+  @else
+    <ul>
+      @foreach( $project->tasks as $task )
+        <li><a href="{{ route('projects.tasks.show', [$project->slug, $task->slug]) }}">{{ $task->name }}</a></li>
+      @endforeach
+    </ul>
+  @endif
 @endsection
