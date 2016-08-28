6ed164de94085a771cbf7529b557598e2f6c4584
Jimmy Havenith <jimhave@outlook.com>
2016-08-29 00:16:33 +0200

    Change color menu scroll

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,19 @@
   {
 
     var menu = $('header');
-    var heightBanner = $('.home-b').height();
+    var heightBanner = $('.home-b').height() - 50;
+
+    $(window).scroll(function()
+    {
+      var scrollOffsetTop = $(window).scrollTop();
+      if(scrollOffsetTop >= heightBanner){
+        $('header').css('background-color', '#1a8c8e');
+        $('header').css('transition', '0.5s');
+      } else if (scrollOffsetTop < heightBanner) {
+        $('header').css('background-color', 'transparent');
+        $('header').css('transition', '0.5s');
+      }
+    });
 
   });
 })(jQuery);
