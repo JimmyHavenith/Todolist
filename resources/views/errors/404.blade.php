564849180887eef1e32b3df3af55465c952595d6
Jimmy Havenith <jimhave@outlook.com>
2016-09-01 20:23:11 +0200

    Page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends ('layout-auth')
+@section('title', 'Connexion' )
+@section('mainContent')
+  <div class="auth">
+    <div class="auth-logo">
+      <h1>minimaList</h1>
+    </div>
+    <div class="auth-auth">
+      <div class="auth-form">
+        <h2>404 Page non trouvée</h2>
+        <span>Oups, cette page n'existe pas</span>
+        <div class="404-link">
+          <a title="Revenir sur la page d'accueil" href="/">Revenir à l'accueil</a>          
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
